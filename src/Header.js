872029c24d6ec489f3046a5192e64baf56f3707a
Hilderal Térence <terencehilderal@gmail.com>
2021-08-19 12:22:05 +0200

    building header section

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+const Header = () => {
+	return (
+		<div className='header'>
+			<h1>This is the header</h1>
+			<div className='header__left'>
+				<img src='' alt='' />
+				<div className='header__search'>
+					{/* search icon */}
+					<input type='text' />
+				</div>
+			</div>
+			<div className='header__right'></div>
+		</div>
+	);
+};
+
+export default Header;
